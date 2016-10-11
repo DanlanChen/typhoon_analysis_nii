d18ab66a5c68213abc02f9be57c8d2a997e7c166
Danlan Chen <danlanchenchen@gmail.com>
2016-10-11 10:51:33 +0900

    Update read_h5.py

--- read_h5.py
+++ read_h5.py
@@ -1,3 +1,4 @@
+#https://www.getdatajoy.com/learn/Read_and_Write_HDF5_from_Python#Reading_Data_from_HDF5_Files
 import numpy as np
 import h5py
 def read_h5(file_name):
@@ -8,4 +9,6 @@ def read_h5(file_name):
         print ('Shape of the array dataset_1: \n', np_data.shape)
 file_name = "/fs9/danlan/typhoon/data/image/201601/2016070318-201601-HMW8-1.h5"
 read_h5(file_name)
-#key "infrared"
+#('List of arrays in this file: \n', [u'infrared'])#key "infrared"
+#('Shape of the array dataset_1: \n', (512, 512))
+
