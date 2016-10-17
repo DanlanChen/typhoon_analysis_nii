40cc8ee93d7f6b6c8134a3a4e101ffdaec5ac9ce
Danlan Chen <danlanchenchen@gmail.com>
2016-10-17 13:58:00 +0900

    Update read_h5.py

--- read_h5.py
+++ read_h5.py
@@ -2,13 +2,20 @@
 import numpy as np
 import h5py
 def read_h5(file_name):
-	with h5py.File(file_name,'r') as hf:
-        print('List of arrays in this file: \n', hf.keys())
+    with h5py.File(file_name,'r') as hf:
+        #print('List of arrays in this file: \n', hf.keys())
         data = hf.get("infrared")
         np_data = np.array(data)
-        print ('Shape of the array dataset_1: \n', np_data.shape)
-file_name = "/fs9/danlan/typhoon/data/image/201601/2016070318-201601-HMW8-1.h5"
-read_h5(file_name)
-#('List of arrays in this file: \n', [u'infrared'])#key "infrared"
-#('Shape of the array dataset_1: \n', (512, 512))
+        #print ('Shape of the array dataset_1: \n', np_data.shape)
+    return np_data
+# def read_h5(file_name):
+# 	with h5py.File(file_name,'r') as hf:
+#         print('List of arrays in this file: \n', hf.keys())
+#         data = hf.get("infrared")
+#         np_data = np.array(data)
+#         print ('Shape of the array dataset_1: \n', np_data.shape)
+# file_name = "/fs9/danlan/typhoon/data/image/201601/2016070318-201601-HMW8-1.h5"
+# read_h5(file_name)
+# #('List of arrays in this file: \n', [u'infrared'])#key "infrared"
+# #('Shape of the array dataset_1: \n', (512, 512))
 
